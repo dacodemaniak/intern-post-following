b4d5b9c7df787497c40a48e36abd6a49bc135736
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-22 08:05:53 +0100

    Structure Directives and Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,64 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html', // View
+  styleUrls: ['./app.component.scss'] // Tableau de SCSS pour la mise en forme
 })
 export class AppComponent {
-  title = 'frontend';
+  public title = 'frontend';
+
+  public stagiaires: any[] = [
+    {
+      firstName: 'Jean-Luc',
+      gender: 'M'
+    },
+    {
+      firstName: 'Oriane',
+      gender: 'F'
+    },
+    {
+      firstName: 'Gabriel',
+      gender: 'M'
+    } // JSON => Java Script Object Notation (Objet JS en plain text)
+  ];
+
+  public showLi: string = 'M';
+
+  public changeGender(): void {
+    if (this.showLi === 'M') {
+      this.showLi = 'F';
+    } else {
+      this.showLi = 'M';
+    }
+  }
+
+  public isShown(stagaire: any): boolean {
+    if (this.showLi === 'A') {
+      return true;
+    }
+
+    if (this.showLi === 'F') {
+      return stagaire.gender === 'F';
+    }
+
+    return stagaire.gender === 'M';
+  }
+
+  public getDisplayRowsNumber(): number {
+    if (this.showLi === 'A') {
+      return this.stagiaires.length;
+    }
+    
+    //return this.stagiaires.filter((stagiaire: any) => stagiaire.gender === this.showLi).length;
+
+    // Avec un for of
+    let displayedItem: number = 0;
+    for (const stagiaire of this.stagiaires) {
+      if (stagiaire.gender === this.showLi) {
+        displayedItem += 1;
+      }
+    }
+    return displayedItem;
+  }
+
 }
