e2b08ab271042458c42e48a76a64d6388581763d
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-23 16:49:21 +0100

    Unit testing for service

--- src/app/core/services/stagiaire-service.spec.ts
+++ src/app/core/services/stagiaire-service.spec.ts
@@ -1,7 +1,24 @@
+import { StagiaireModel } from '../models/stagiaire-model';
 import { StagiaireService } from './stagiaire-service';
 
 describe('StagiaireService', () => {
-  it('should create an instance', () => {
-    expect(new StagiaireService()).toBeTruthy();
+  let service: StagiaireService;
+
+  beforeEach(() => {
+    service = new StagiaireService();
+    service.deserialize();
   });
+
+  it('should create an instance', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it(`Should throw an error if id was not found`, () => {
+    expect(() => service.findOne(12)).toThrowError();
+  });
+
+  it(`Should return 'Aubert' if id was 1`, () => {
+    const stagiaire: StagiaireModel = service.findOne(1);
+    expect(stagiaire.lastName).toBe('Aubert');
+  })
 });
