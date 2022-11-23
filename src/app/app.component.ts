3fb6991985152f8f66dd22289b0ffab2ebee4c07
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-23 13:20:36 +0100

    Refactor AppComponent to add router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { StagiaireModel } from './core/models/stagiaire-model';
-import { StagiaireService } from './core/services/stagiaire-service';
 
 @Component({
   selector: 'app-root',
@@ -10,51 +8,6 @@ import { StagiaireService } from './core/services/stagiaire-service';
 export class AppComponent {
   public title = 'frontend';
 
-  public stagiaires: StagiaireModel[] = [];
-
-  public showLi: string = 'M';
-
-  public constructor() {
-    const service: StagiaireService  = new StagiaireService();
-    service.deserialize();
-    this.stagiaires = service.getStagiaires();
-  }
-
-  public changeGender(): void {
-    if (this.showLi === 'M') {
-      this.showLi = 'F';
-    } else {
-      this.showLi = 'M';
-    }
-  }
-
-  public isShown(stagaire: any): boolean {
-    if (this.showLi === 'A') {
-      return true;
-    }
-
-    if (this.showLi === 'F') {
-      return stagaire.gender === 'F';
-    }
-
-    return stagaire.gender === 'M';
-  }
-
-  public getDisplayRowsNumber(): number {
-    if (this.showLi === 'A') {
-      return this.stagiaires.length;
-    }
-    
-    //return this.stagiaires.filter((stagiaire: any) => stagiaire.gender === this.showLi).length;
-
-    // Avec un for of
-    let displayedItem: number = 0;
-    for (const stagiaire of this.stagiaires) {
-      if (stagiaire.gender === this.showLi) {
-        displayedItem += 1;
-      }
-    }
-    return displayedItem;
-  }
+  public constructor() {}
 
 }
