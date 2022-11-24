ec810a546584f6f1740f749dd8eb6fe20b4e2670
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-24 13:11:10 +0100

    Import SharedModule to benefit HttpClientModule

--- src/app/stagiaires/stagiaires.module.ts
+++ src/app/stagiaires/stagiaires.module.ts
@@ -5,6 +5,7 @@ import { StagiairesRoutingModule } from './stagiaires-routing.module';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 import { InitialsPipe } from './pipes/initials.pipe';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { InitialsPipe } from './pipes/initials.pipe';
   ],
   imports: [
     CommonModule,
-    StagiairesRoutingModule
+    StagiairesRoutingModule,
+    SharedModule
   ]
 })
 export class StagiairesModule { }
