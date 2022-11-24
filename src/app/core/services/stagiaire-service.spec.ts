e89262ea35f6f8c309fcd4bec6344b73b401b0fa
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-24 14:07:42 +0100

    Refactor httpClient service testing

--- src/app/core/services/stagiaire-service.spec.ts
+++ src/app/core/services/stagiaire-service.spec.ts
@@ -1,24 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { StagiaireModel } from '../models/stagiaire-model';
 import { StagiaireService } from './stagiaire-service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { doesNotReject } from 'assert';
+import { environment } from 'src/environments/environment';
 
 describe('StagiaireService', () => {
+  let httpClient: HttpClient;
   let service: StagiaireService;
 
   beforeEach(() => {
-    service = new StagiaireService();
-    service.deserialize();
+    TestBed.configureTestingModule(
+      {
+        imports: [
+          SharedModule,
+          HttpClientTestingModule
+        ],
+        providers: [
+          HttpClient
+        ]
+      }
+    )
+    httpClient = TestBed.inject(HttpClient);
+    service = new StagiaireService(httpClient);
   });
 
   it('should create an instance', () => {
     expect(service).toBeTruthy();
   });
 
-  it(`Should throw an error if id was not found`, () => {
-    expect(() => service.findOne(12)).toThrowError();
-  });
+  it(`Should return an Observable of a StagiaireModel array`, () => {
+    const httpTestingController = TestBed.inject(HttpTestingController)
+    service.findAll()
+      .subscribe((stagiaires: StagiaireModel[]) => {
+        expect(stagiaires).toBeInstanceOf(Observable);
+      });
+    
+    const request = httpTestingController.expectOne(
+      `${environment.fakeApi}stagiaires`
+    );
 
-  it(`Should return 'Aubert' if id was 1`, () => {
-    const stagiaire: StagiaireModel = service.findOne(1);
-    expect(stagiaire.lastName).toBe('Aubert');
+    expect(request.request.method).toBe('GET');
+    httpTestingController.verify();
   })
 });
