fc13826d4014f0d733f099a42265bec8f4c53c9d
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-24 14:00:28 +0100

    Inject StagiaireService to stagiaire components testing

--- src/app/stagiaires/detail/detail.component.spec.ts
+++ src/app/stagiaires/detail/detail.component.spec.ts
@@ -1,7 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppModule } from 'src/app/app.module';
+import { StagiaireService } from 'src/app/core/services/stagiaire-service';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { DetailComponent } from './detail.component';
 
@@ -9,11 +12,12 @@ describe('DetailComponent', () => {
   let component: DetailComponent;
   let fixture: ComponentFixture<DetailComponent>;
   let route: ActivatedRoute;
+  let stagiaireService: StagiaireService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ DetailComponent ],
-      imports: [ RouterTestingModule, AppModule ],
+      imports: [ RouterTestingModule, AppModule, SharedModule ],
     })
     .compileComponents();
   });
@@ -21,6 +25,7 @@ describe('DetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DetailComponent);
     route = TestBed.inject(ActivatedRoute);
+    stagiaireService = TestBed.inject(StagiaireService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/stagiaires/list/list.component.spec.ts
+++ src/app/stagiaires/list/list.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppModule } from 'src/app/app.module';
+import { StagiaireService } from 'src/app/core/services/stagiaire-service';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { InitialsPipe } from 'src/app/stagiaires/pipes/initials.pipe';
 
 import { ListComponent } from './list.component';
@@ -9,17 +11,19 @@ import { ListComponent } from './list.component';
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let stagiaireService: StagiaireService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ListComponent, InitialsPipe ],
-      imports: [ AppModule, RouterTestingModule ],
+      imports: [ AppModule, RouterTestingModule, SharedModule ],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListComponent);
+    TestBed.inject(StagiaireService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
