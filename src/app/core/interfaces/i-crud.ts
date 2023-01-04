407b51541f7ecb30cfcd737c7dcfa7ab2153d559
Jean-Luc <jean-luc.a@ideafactory.fr>
2023-01-04 14:23:02 +0100

    Create ICrud interface
    Implements this interface to all CRUD services

---
+++ src/app/core/interfaces/i-crud.ts
@@ -0,0 +1,40 @@
+import { Observable } from "rxjs";
+
+export interface ICrud<T> {
+  /**
+   * findAll()
+   * @returns An Observable of an array of T
+   *  T is a generic, must be defined in services
+   *  that implements this Interface
+   */
+  findAll(): Observable<T[]>;
+
+  /**
+   * findOne(id: number)
+   * @param id id of the T object i want to Observe
+   * @returns Observable of a T object
+   */
+  findOne(id: number): Observable<T>;
+
+  /**
+   * create(datas: any) Insert a new Object in the database
+   * @param datas Datas representing a T object
+   * @returns T object after creation
+   */
+  create(datas: any): Observable<T>;
+
+  /**
+   * update(datas: any) Update an Object in the database
+   * @param datas Datas representing Object to update in the database
+   * @returns void
+   */
+  update(datas: T): void;
+
+  /**
+   * delete(datas: T) Remove an Object from the database
+   * @param datas Object i want to delete in the database
+   * @returns void
+   */
+  delete(datas: T): void;
+
+}
