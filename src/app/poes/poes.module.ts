bc236596a54ed3181139deb7679ba035360d3806
Jean-Luc <jean-luc.a@ideafactory.fr>
2023-01-04 11:05:35 +0100

    Create list component Update poes module

--- src/app/poes/poes.module.ts
+++ src/app/poes/poes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PoesRoutingModule } from './poes-routing.module';
+import { ListComponent } from './components/list/list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListComponent
+  ],
   imports: [
     CommonModule,
     PoesRoutingModule
