b3190fa91e5e6bedd538a9c98baf3e6eacff4c38
Jean-Luc <jean-luc.a@ideafactory.fr>
2023-01-04 11:08:37 +0100

    Create Manage component Mean update and add handlers

--- src/app/poes/poes.module.ts
+++ src/app/poes/poes.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PoesRoutingModule } from './poes-routing.module';
 import { ListComponent } from './components/list/list.component';
+import { ManageComponent } from './components/manage/manage.component';
 
 
 @NgModule({
   declarations: [
-    ListComponent
+    ListComponent,
+    ManageComponent
   ],
   imports: [
     CommonModule,
