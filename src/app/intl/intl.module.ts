bf8e60e60931aaa70a8146c3b13bdb0b72ed33eb
Jean-Luc <jean-luc.a@ideafactory.fr>
2023-01-10 11:33:50 +0100

    Add located strings for TranslateModule

--- src/app/intl/intl.module.ts
+++ src/app/intl/intl.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { intlProvider } from './services/intl.service';
+import { intlProvider, IntlService } from './services/intl.service';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule.forRoot(
+      {
+        loader: {
+          provide: TranslateLoader,
+          useFactory: IntlService.httpLoaderFactory,
+          deps: [
+            HttpClient
+          ]
+        }
+      }
+    )
+  ],
+  exports: [
+    TranslateModule
   ],
   providers: [
     intlProvider
--- src/app/intl/services/intl.service.ts
+++ src/app/intl/services/intl.service.ts
@@ -1,31 +1,82 @@
-import { APP_INITIALIZER, Injectable } from '@angular/core';
+import { LOCATION_INITIALIZED } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { APP_INITIALIZER, Injectable, Injector } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IntlService {
 
-  constructor() { }
+  private _language: string = '';
+  private _translateService!: TranslateService;
 
-  init(): Promise<void> {
-    return new Promise((resolve: any) => {
-      console.log(`I'm running`);
-      resolve();
-    });
+  constructor() { };
+
+  get language(): string {
+    return this._language;
   }
 
-  public static appInitializer(intlService: IntlService) {
+  set language(language: string) {
+    this._language = language;
+  }
+
+  init(translateService: TranslateService, injector: Injector): Promise<void> {
+    return new Promise((resolve: any) => {
+      injector.get(
+        LOCATION_INITIALIZED,
+        Promise.resolve(null) // If TOKEN was not provided
+      ).then(() => { // Angular says i'm okay with my own resources, do what u need
+        const userLanguage: string = window.navigator.language.split('-')[0]; // fr-FR => fr | FR => fr
+
+        this._language = /(fr|en|de|it|es)/gi.test(userLanguage) ? userLanguage : 'fr';
+
+        this._translateService = translateService;
+
+        this._switchLanguage().subscribe(() => {
+          console.log(`Location was loaded for ${this._language}`);
+          resolve();
+        })
+
+      })
+
+    });
+
+  }
+
+  private _switchLanguage(): Observable<any> {
+    return this._translateService.use(this._language)
+  }
+
+  public static appInitializer(
+    intlService: IntlService,
+    translateService: TranslateService,
+    injector: Injector
+  ) {
     return (): Promise<void> => {
-      return intlService.init();
+      return intlService.init(translateService, injector);
     }
   }
+
+  public static httpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
+    return new TranslateHttpLoader(
+      httpClient,
+      './assets/i18n/',
+      '.json'
+    );
+  }
+
 }
 
 export const intlProvider = {
   provide: APP_INITIALIZER, // L'application démarre
   useFactory: IntlService.appInitializer,
   deps: [
-    IntlService
+    IntlService,
+    TranslateService,
+    Injector
   ],
   multi: true
 }
