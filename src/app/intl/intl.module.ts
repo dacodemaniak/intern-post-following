1281420d75ee507fc65a041bc785833f3d422a38
Jean-Luc <jean-luc.a@ideafactory.fr>
2023-01-10 10:02:20 +0100

    Provide factory before application starts

--- src/app/intl/intl.module.ts
+++ src/app/intl/intl.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { intlProvider } from './services/intl.service';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    intlProvider
   ]
 })
 export class IntlModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { UiModule } from '../ui/ui.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DateAdapter, MatNativeDateModule, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { IntlModule } from '../intl/intl.module';
 
 @NgModule({
   declarations: [],
@@ -16,7 +17,8 @@ import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS, MomentDateAda
     ReactiveFormsModule,
     UiModule,
     // @todo Move to UIModule
-    MatNativeDateModule
+    MatNativeDateModule,
+    IntlModule
   ],
   providers: [
     // The locale would typically be provided on the root module of your application. We do it at
